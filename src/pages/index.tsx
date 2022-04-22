49d0a690887c6aaa3d6070c1d0c13ae9def66e61
Thyago Carvalho <othyagocarvalho@outlook.com>
2022-04-22 10:44:28 -0300

    Finish Home Page
    Add Hero with text and image.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,28 @@
-/* import  styles from '../styles/home.module.scss' */
+import  styles from './home.module.scss';
 import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
-       <Head>
+      <Head>
          <title>
-          Início Ignews
-         </title>
-        
+          Home | Ig.news
+         </title>        
       </Head>
-      <h1 /* className={styles.title} */>      
-        Hello World
-      </h1>
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span> 🖐 Hey, Welcome </span>
+          <h1>News about the <span> React</span> world.</h1>
+          <p> 
+            Get access to all the publications <br/>
+            <span> for $ 9.99 month</span>
+          </p>
+        </section>
+
+        <img src="images/avatar.svg" alt="woman coding" />
+
+      </main>
     </>
    
   )
